5c0bd009fed7b3d4b86383ea110d511d00519946
Peete <peetemusic@gmail.com>
2021-02-14 17:30:08 -0500

    2nd changes

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,7 +17,7 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::firebrick);
+    g.setColour (juce::Colours::greenyellow);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
